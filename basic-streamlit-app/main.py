7bbd93e5b7b155c2bf2751b58c38b302db6c28e2
ArisC123 <ariscardoso87@gmail.com>
2025-02-09 04:25:20 -0500

    New app updates

--- basic-streamlit-app/main.py
+++ basic-streamlit-app/main.py
@@ -2,14 +2,15 @@ import streamlit as st
 import pandas as pd
 
 st.title("Welcome to the Penguin Disovery Dashboard!")
-
+st.divider()
 data = pd.read_csv("Data/penguins.csv")
 
-#st.write("Here's a sneak peak of our dataset:")
-#st.dataframe(data.head(5))
+st.write("Here's a sneak peak of our dataset:")
+st.dataframe(data.head())
 
-##Summary Statistics Portion
-st.subheader("Penguin Trait Averages")
+
+#Summary Statistics Portion
+st.header("Penguin Trait Averages")
 species = st.selectbox("Select the species", data['species'].unique())
 island = st.selectbox("Select the island", data['island'].unique())
 year = st.selectbox("Select the year", data['year'].unique())
@@ -18,18 +19,42 @@ sex = st.selectbox("Select the gender", data['sex'].unique())
 filtered_data = data[(data['species'] == species) & (data['island'] == island) & (data['year'] == year) & 
                      (data['sex'] == sex)]
 
-#st.dataframe(filtered_data.select_dtypes(include = ['number']).describe())  
-
-#st.metric(label="Count", value= f'{filtered_data.shape[0]}')
-
-#label="Count", value= f'{round(filtered_data['bill_length_mm'].mean(),2)}')
-
 col1, col2,col3, col4, col5 = st.columns(5)
 col1.metric('Count', f'{filtered_data.shape[0]}')
 col2.metric('Bill Length', f'{round(filtered_data["bill_length_mm"].mean(),1)}')
 col3.metric('Bill Depth', f'{round(filtered_data["bill_depth_mm"].mean(),1)}')
 col4.metric('Flipper Length', f'{round(filtered_data["flipper_length_mm"].mean(),1)}')
 col5.metric('Body Mass', f'{round(filtered_data["body_mass_g"].mean(),1)}')
+
+st.divider()
+#Data Visualization Portion
+st.header("Data Visualizations")
+
+col6, col7 = st.columns(2)
+
+species_count = data['species'].value_counts()
+with col6:
+    st.bar_chart(species_count, y_label='Species', x_label='Count', color = '#CFA8FF', width = 200, height = 300,
+                 horizontal= True)
+island_porportion = data['island'].value_counts(normalize=True)
+with col7: 
+    st.bar_chart(island_porportion*100, y_label='Island', x_label='Count', color = '#FFC0CB', width = 200, height = 300,
+                 stack = True) 
+
+species_hist = st.selectbox('Select a species', data['species'].unique())
+sex_hist = st.selectbox('Select a gender', data['sex'].unique())
+
+hist_data = data[(data['species'] == species_hist) & (data['sex'] == sex_hist)] 
+st.scatter_chart(hist_data, x = 'flipper_length_mm', y = 'body_mass_g', x_label = 'Flipper Length',
+                 y_label = 'Body Mass(g)', width = 10, height = 300)
+
+
+gender_make = data['sex'].value_counts()
+st.bar_chart(gender_make, x_label ='Sex', y_label = 'Count', color = '#9CAF88', width = 5, height = 300)
+
+
+
+
 
 
 
