cd016275fe3b69802a50c9b3060f959a957ae5d8
ArisC123 <117526306+ArisC123@users.noreply.github.com>
2025-03-10 00:56:03 -0500

    Update main.py

--- TidyData-Project/main.py
+++ TidyData-Project/main.py
@@ -1,1 +1,14 @@
+# Applying the data cleaning concepts 
+# Output the original untidy data frame
+
+import pandas as pd
+
+print("Original (untidy) Federal R&D budget data frame ")
+df = pd.read_csv("Data/fed_rd_year&gdp.csv")
+print(df)
+
+
+# Melt the DataFrame: convert subject columns of years into a single 'year' column,
+# with their corresponding budgets in a new 'Budget' column
+
 
