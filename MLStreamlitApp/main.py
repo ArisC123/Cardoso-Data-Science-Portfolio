8367593bf5372b8dbbd55f5640f11d31005db0cd
ArisC123 <ariscardoso87@gmail.com>
2025-04-14 19:33:04 -0400

    Nothing

--- MLStreamlitApp/main.py
+++ MLStreamlitApp/main.py
@@ -4,7 +4,7 @@ import seaborn as sns
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import accuracy_score, classification_report
+from sklearn.metrics import accuracy_score
 from sklearn.linear_model import LogisticRegression
 import matplotlib.pyplot as plt
 from sklearn.metrics import roc_curve, roc_auc_score
